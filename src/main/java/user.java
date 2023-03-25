5351f2c5ee699f8f23b74780698b49285b053d00
olfasalem <olfasalem2021@gmail.com>
2023-03-25 14:17:55 +0100

    first commit

---
+++ src/main/java/user.java
@@ -0,0 +1,39 @@
+public class user {
+    private int id ;
+    private String name ;
+    private String email;
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    private String Password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
